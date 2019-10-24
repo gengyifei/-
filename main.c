928ceec7500b2638b5f215d604bd198e5c9c0d9f
gengyifei <39208098+gengyifei@users.noreply.github.com>
2019-10-24 17:17:03 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,40 @@
+/*
+ * maxstr.c
+ *
+ *  Created on: 2019年10月24日
+ *      Author: gyf
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int lengthOfLongestSubstring(char * s) {
+    int table[0xFF];
+    memset(table, -1, sizeof(table));
+    int maxlen = 0;
+    int idx = 0;
+    int startidx = 0;
+    int findidx = 0;
+    int ch;
+    while ((ch = s[idx]) != '\0') {
+
+        findidx = table[ch];
+        if (findidx >= startidx) {
+            int len = idx - startidx;
+            maxlen = len > maxlen ? len : maxlen;
+            startidx = findidx + 1;
+        }
+        table[ch] = idx;
+        ++idx;
+    }
+    int len = idx - startidx;
+    maxlen = len > maxlen ? len : maxlen;
+    return maxlen;
+}
+
+int main()
+{
+    printf("%d\n", lengthOfLongestSubstring("123a3cnbj3"));
+}
